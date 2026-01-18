027174c2f4b3c232cc781773208f9d6b17c89f61
Brady McDonough <me@bradymcd.ca>
2026-01-18 12:33:10 -0700

    Changed gravity modification mixin to be more compatible. No longer crashes when included with quoteall's gravity changer API.

--- src/main/java/paulevs/edenring/mixin/common/LivingEntityMixin.java
+++ src/main/java/paulevs/edenring/mixin/common/LivingEntityMixin.java
@@ -1,20 +1,19 @@
 package paulevs.edenring.mixin.common;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import paulevs.edenring.EdenRing;
 import paulevs.edenring.world.GravityController;
 
-@Mixin(LivingEntity.class)
+@Mixin(value = LivingEntity.class)
 public class LivingEntityMixin {
-	@ModifyConstant(method = "travel", constant = @Constant(doubleValue = 0.08D))
+	@ModifyExpressionValue(method = "travel", at = @At( value = "CONSTANT", args = "doubleValue=0.08D"))
 	private double eden_changeGravity(double gravity) {
 		LivingEntity entity = LivingEntity.class.cast(this);
 		if (entity.level().dimension() == EdenRing.EDEN_RING_KEY) {
